38923d14ca569b9117cd16d6e9a0964bac23dfe5
taektu_coding <taektu.coding@gmail.com>
2025-05-09 10:26:41 +0200

    added: startScreen sound

--- screens/start-screen.js
+++ screens/start-screen.js
@@ -14,24 +14,27 @@ class StartScreen {
     this.startGameCallback = startGameCallback;
     this.showControlsCallback = showControlsCallback;
     this.backgroundImage = new Image();
-    this.title = "Order of the Chosen";
+    this.backgroundImage.src = "assets/img/ui/menu_bg.png";
     this.startButton = {
       label: "Start Game",
-      x: 100,
-      y: 300,
+      x: canvas.width / 2 - 110,
+      y: canvas.height / 2 - 75,
       width: 200,
       height: 50,
     };
     this.controlsButton = {
       label: "Controls",
-      x: 420,
-      y: 300,
+      x: canvas.width / 2 - 110,
+      y: canvas.height / 2,
       width: 200,
       height: 50,
     };
     this.setBindings();
     this.addEventListeners();
-    this.backgroundImage.onload = () => this.draw();
+    this.backgroundImage.onload = () => {
+      this.backgroundImageLoaded = true;
+      this.draw();
+    };
   }
 
   setBindings() {
@@ -54,7 +57,6 @@ class StartScreen {
 
   draw() {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    this.ctx.fillStyle = "lightblue"; // Beispiel-Hintergrundfarbe
     this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
     if (this.backgroundImage.complete) {
       this.ctx.drawImage(
@@ -65,10 +67,6 @@ class StartScreen {
         this.canvas.height,
       );
     }
-    this.ctx.font = "48px serif";
-    this.ctx.fillStyle = "white";
-    this.ctx.textAlign = "center";
-    this.ctx.fillText(this.title, this.canvas.width / 2, 100);
 
     this.drawButton(this.startButton);
     this.drawButton(this.controlsButton);
